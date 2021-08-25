1b2c4c420a8fdd200ad565a1b897b3dd053e5e69
Thiago Teixeira <thiago@streamlit.io>
2021-08-25 08:55:06 -0700

    Improve time_hist tickBand and also accept spec for date

--- plost/__init__.py
+++ plost/__init__.py
@@ -812,8 +812,8 @@ def time_hist(
         height=height,
         title=title,
         encoding=_(
-            x=_(field=date, timeUnit=x_unit, title=None),
-            y=_(field=date, timeUnit=y_unit, title=None),
+            x=_clean_encoding(data, date, timeUnit=x_unit, title=None, axis=_(tickBand='extent')),
+            y=_clean_encoding(data, date, timeUnit=y_unit, title=None, axis=_(tickBand='extent')),
             color=_clean_encoding(data, color, aggregate=aggregate, legend=legend)
         ),
         selection=_get_selection(pan_zoom),
